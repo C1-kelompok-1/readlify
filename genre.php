12c5cd190e4529818f4835bd02ba69becc23f4b3
novilfahlevy <novilfreon@gmail.com>
2023-04-29 19:31:41 +0800

    halaman novel berdasarkan genre

---
+++ genre.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Readify</title>
+
+    <?php require 'layouts/styles.php'; ?>
+
+</head>
+
+<body>
+
+    <main>
+        <?php require 'layouts/navbar.php'; ?>
+
+        <header class="site-header d-flex flex-column justify-content-center align-items-center">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12 col-12 text-center">
+                <h2 class="mb-0">Pilih genre kesukaanmu</h2>
+              </div>
+            </div>
+          </div>
+        </header>
+
+        <section class="section-padding">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12 col-12">
+                <div class="section-title-wrap mb-5">
+                  <a href="#" class="btn custom-btn section-title me-3">Drama</a>
+                  <a href="#" class="btn custom-btn section-title me-3">Romantis</a>
+                  <a href="#" class="btn custom-btn section-title me-3">Fantasi</a>
+                  <a href="#" class="btn custom-btn section-title me-3">Komedi</a>
+                  <a href="#" class="btn custom-btn section-title">Aksi</a>
+                </div>
+              </div>
+              <div class="col-lg-4 col-12 mb-4 mb-lg-0">
+                <div class="custom-block custom-block-full">
+                  <div class="custom-block-image-wrap">
+                    <a href="detail-page.php">
+                      <img src="https://cdn.gramedia.com/uploads/items/9786239726218.jpg" class="custom-block-image img-fluid" alt="">
+                    </a>
+                  </div>
+
+                  <div class="custom-block-info">
+                    <!-- Judul -->
+                    <h5 class="mb-2">
+                      <a href="detail-page.php">
+                        Bedebah Diujung Tanduk
+                      </a>
+                    </h5>
+
+                    <!-- Nama penulis -->
+                    <div class="profile-block d-flex">
+                      <p>Elsa</p>
+                    </div>
+
+                    <!-- Sipnosis -->
+                    <p class="mb-0">Lorem Ipsum dolor sit amet consectetur</p>
+
+                    <!-- Suka -->
+                    <div class="custom-block-bottom d-flex justify-content-between mt-3">
+                      <a href="#" class="bi-heart me-1">
+                        <span>2.5k</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-12 mb-4 mb-lg-0">
+                <div class="custom-block custom-block-full">
+                  <div class="custom-block-image-wrap">
+                    <a href="detail-page.php">
+                      <img src="https://cdn.gramedia.com/uploads/items/9786239726218.jpg" class="custom-block-image img-fluid" alt="">
+                    </a>
+                  </div>
+
+                  <div class="custom-block-info">
+                    <!-- Judul -->
+                    <h5 class="mb-2">
+                      <a href="detail-page.php">
+                        Bedebah Diujung Tanduk
+                      </a>
+                    </h5>
+
+                    <!-- Nama penulis -->
+                    <div class="profile-block d-flex">
+                      <p>Elsa</p>
+                    </div>
+
+                    <!-- Sipnosis -->
+                    <p class="mb-0">Lorem Ipsum dolor sit amet consectetur</p>
+
+                    <!-- Suka -->
+                    <div class="custom-block-bottom d-flex justify-content-between mt-3">
+                      <a href="#" class="bi-heart me-1">
+                        <span>2.5k</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-12 mb-4 mb-lg-0">
+                <div class="custom-block custom-block-full">
+                  <div class="custom-block-image-wrap">
+                    <a href="detail-page.php">
+                      <img src="https://cdn.gramedia.com/uploads/items/9786239726218.jpg" class="custom-block-image img-fluid" alt="">
+                    </a>
+                  </div>
+
+                  <div class="custom-block-info">
+                    <!-- Judul -->
+                    <h5 class="mb-2">
+                      <a href="detail-page.php">
+                        Bedebah Diujung Tanduk
+                      </a>
+                    </h5>
+
+                    <!-- Nama penulis -->
+                    <div class="profile-block d-flex">
+                      <p>Elsa</p>
+                    </div>
+
+                    <!-- Sipnosis -->
+                    <p class="mb-0">Lorem Ipsum dolor sit amet consectetur</p>
+
+                    <!-- Suka -->
+                    <div class="custom-block-bottom d-flex justify-content-between mt-3">
+                      <a href="#" class="bi-heart me-1">
+                        <span>2.5k</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+    </main>
+
+    <?php require 'layouts/footer.php'; ?>
+
+    <?php require 'layouts/scripts.php'; ?>
+
+</body>
+
+</html>
