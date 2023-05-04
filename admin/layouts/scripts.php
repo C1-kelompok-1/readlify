f8b0aab06dbae4b68d7126440f2e7f0db4976d62
novilfahlevy <novilfreon@gmail.com>
2023-05-04 21:43:17 +0800

    templating admin

---
+++ admin/layouts/scripts.php
@@ -0,0 +1,7 @@
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/pace.min.js"></script>
+<script src="assets/js/sidebar-menu.js"></script>
+<script src="assets/js/app-script.js"></script>
+<script src="assets/js/index.js"></script>
