366bf6ebac1c6fc2fef6fade7f4fa9822bb2ff07
karlen <karlenderawan07@gmail.com>
2023-05-06 12:43:04 +0800

    membuat fungsi login dan register

--- login.php
+++ login.php
@@ -1,3 +1,46 @@
+<?php
+require "session.php";
+include('koneksi.php');
+if (isset($_POST['login'])) {
+  // Get input values
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $role = $_POST['role'];
+
+  // Check user credentials in database
+  $query = "SELECT * FROM pengguna WHERE username = '$username' AND role = '$role'";
+  $result = mysqli_query($conn, $query);
+
+  if (mysqli_num_rows($result) > 0) {
+    $user = mysqli_fetch_assoc($result);
+
+
+
+
+    // if(password_verify($password, $user['password'])) {
+    // if ($password == $user['password']) {
+    if (password_verify($password, $user['password']) || $password == $user['password']) {
+
+      $_SESSION['user'] = $user['username'];
+      $_SESSION['role'] = $user['role'];
+
+      if ($role == 'Pembaca') {
+        header('Location: index.php');
+      } else if ($role == 'Penulis') {
+        header('Location: koin.php');
+      } else if ($role == 'Admin') {
+        header('Location: admin/index.php');
+      }
+    } else {
+      echo "<script>alert('Password yang Anda masukkan salah.');</script>";
+      // echo "Password yang Anda masukkan salah. Password yang dimasukkan: $password. Password yang tersimpan di database: {$user['password']}";
+
+    }
+  } else {
+    echo "<script>alert('Username tidak ditemukan.');</script>";
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,21 +57,23 @@
 <body>
   <div class="container" id="container">
     <div class="form-container login-container">
-      <form action="#">
+
+      <form action="" method="POST">
         <h1>Masuk</h1>
-        <input type="text" placeholder="Email atau username" />
-        <input type="password" placeholder="Password" />
+        <input type="text" name="username" placeholder="Email atau username" />
+        <input type="password" name="password" placeholder="Password" />
         <select name="role">
           <option value="Pembaca">Pembaca</option>
           <option value="Penulis">Penulis</option>
+          <option value="Admin">Admin</option>
         </select>
-        <button>Masuk</button>
         <div class="content">
           <div class="checkbox">
             <input type="checkbox" name="checkbox" id="checkbox" />
             <label>Ingat saya</label>
           </div>
         </div>
+        <button type="submit" name="login">Masuk</button>
       </form>
     </div>
 
@@ -49,4 +94,5 @@
 
   <?php require 'layouts/auth/scripts.php' ?>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -1,3 +1,70 @@
+<?php
+include('koneksi.php');
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+function getInputValue($name)
+{
+  if (isset($_POST[$name])) {
+    echo $_POST[$name];
+  }
+}
+// Fungsi untuk menambahkan user ke database
+function tambahUser($conn, $email, $username, $password, $role)
+{
+  // Cek apakah email atau username sudah ada di database
+  $query = "SELECT * FROM pengguna WHERE email = '$email' OR username = '$username'";
+  $result = mysqli_query($conn, $query);
+
+  if (mysqli_num_rows($result) > 0) {
+    // Jika sudah ada, kembalikan false
+    return false;
+  } else {
+    // Jika belum ada, tambahkan pengguna ke database
+    $sql = "INSERT INTO pengguna (role, username, password, email) VALUES ('$role', '$username', '$password', '$email')";
+    $query = mysqli_query($conn, $sql);
+    return $query;
+  }
+}
+
+// Cek apakah tombol submit sudah di klik
+if (isset($_POST["daftar"])) {
+  $email = $_POST["email"];
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+  $konfirmasipw = $_POST["konfirmasipw"];
+  $role = $_POST["role"];
+
+
+  if (empty($email) || empty($username) || empty($password) || empty($konfirmasipw)) {
+    echo "<script>alert('Email, username, password, dan konfirmasi password harus diisi!')</script>";
+    header('Location: register.php');
+  } else {
+
+    // // Cek apakah password dan konfirmasi password sama
+    if ($password === $konfirmasipw) {
+      // Enkripsi password
+      $password = password_hash($password, PASSWORD_DEFAULT);
+
+      // Tambahkan user ke database
+      $result = tambahUser($conn, $email, $username, $password, $role);
+      if ($result) {
+        $_SESSION['success'] = "Registrasi berhasil. Silakan login.";
+        echo "<script>alert('Registrasi berhasil. Silakan login.')</script>";
+        header('Location: login.php');
+      } else {
+        // $_SESSION['error'] = "Registrasi gagal. Silakan coba lagi.";
+        echo "<script>alert('Email atau username sudah terdaftar. Silakan coba lagi.')</script>";
+        // header('Location: register.php');
+      }
+    } else {
+      echo "<script>alert('Password dan konfirmasi password tidak sama!.')</script>";
+    }
+  }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,33 +81,17 @@
 <body>
   <div class="container" id="container">
     <div class="form-container register-container">
-      <form action="#">
-        <h1>Register</h1>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <select name="role">
-          <option value="Reader">Reader</option>
-          <option value="Creator">Creator</option>
-        </select>
-        <button>Register</button>
-        <span>or use your account</span>
-        <div class="social-container">
-          <a href="#" class="social"><img src="assets/images/social/facebook.svg" alt="" /><i class="lni lni-facebook-fill"></i></a>
-          <a href="#" class="social"><img src="assets/images/social/google.svg" alt="" /><i class="lni lni-google"></i></a>
-          <a href="#" class="social"><img src="assets/images/social/twitter.svg" alt="" /><i class="lni lni-linkedin-original"></i></a>
-        </div>
-      </form>
-    </div>
-
-    <div class="form-container login-container">
-      <form action="#">
+      <form action="#" method="post">
         <h1>Buat akun</h1>
-        <input type="email" placeholder="Email" />
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Konfirmasi password" />
-        <button>Daftar</button>
+        <input type="email" name="email" placeholder="Email" />
+        <input type="text" name="username" placeholder="Username" />
+        <input type="password" name="password" placeholder="Password" />
+        <input type="password" name="konfirmasipw" placeholder="Konfirmasi password" />
+        <select name="role">
+          <option value="Pembaca">Pembaca</option>
+          <option value="Penulis">Penulis</option>
+        </select>
+        <button type="submit" name="daftar">Daftar</button>
       </form>
     </div>
 
@@ -64,4 +115,5 @@
 
   <?php require 'layouts/auth/scripts.php' ?>
 </body>
+
 </html>
